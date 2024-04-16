d3569268235b10812f4dc0412196a3413653946d
unknown <juosmas@unbosque.edu.co>
2024-04-15 20:50:32 -0500

    Final de pagina mi-solicitud

--- src/components/ComponentChat.jsx
+++ src/components/ComponentChat.jsx
@@ -1,6 +1,9 @@
-import { useState } from 'react';
-import { MessageOutlined, UserOutlined, SendOutlined } from '@ant-design/icons';
+import { useState, useEffect } from 'react';
 import './ComponentChat.css';
+import { PiUserCircleBold } from "react-icons/pi";
+import { RiMessage3Fill } from "react-icons/ri";
+import { IoSend } from "react-icons/io5";
+import { RiCloseCircleFill } from "react-icons/ri";
 
 // Componente de chat
 const ChatComponent = () => {
@@ -9,8 +12,31 @@ const ChatComponent = () => {
   const [replyMessage, setReplyMessage] = useState('');
   const [replyingTo, setReplyingTo] = useState(null); // Guarda el mensaje al que se está respondiendo
   const [isReplying, setIsReplying] = useState(false); // Estado para controlar si se está respondiendo
+  //const [currentUser, setCurrentUser] = useState(null);
 
-  const currentUser = 'User1'; // Nombre del usuario actual, puedes cambiar esto según tu lógica
+
+  {/*useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const response = await fetch('http://tu-servidor-backend.com/api/user', { //cambiar
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+        const userData = await response.json();
+        setCurrentUser(userData);
+      } catch (error) {
+        console.error('Error al obtener información del usuario:', error);
+      }
+    };
+
+    // Llama a la función para obtener la información del usuario al cargar el componente
+    fetchUserData();
+  }, []);*/}
+
+  const currentUser = 'User1';
+
 
   const handleInputChange = (e) => {
     setInputMessage(e.target.value);
@@ -42,21 +68,43 @@ const ChatComponent = () => {
         text: replyMessage,
         sender: currentUser,
         timestamp: new Date().toISOString(),
-        repliedTo: repliedMessage, 
+        repliedTo: repliedMessage,
       };
       setMessages([...messages, newMessage]);
       setReplyMessage('');
-      setReplyingTo(null); // Limpiar el estado de mensaje al que se está respondiendo
-      setIsReplying(false); // Ocultar el input y el botón de respuesta
+      setReplyingTo(null);
+      setIsReplying(false);
     }
   };
 
+
+  useEffect(() => {
+    // Función para enviar los mensajes al backend (ajusta la URL según tu configuración)
+    const sendMessagesToBackend = async () => {
+      try {
+        const response = await fetch('http://tu-servidor-backend.com/api/messages', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(messages),
+        });
+        const data = await response.json();
+        console.log('Respuesta del servidor:', data);
+      } catch (error) {
+        console.error('Error al enviar mensajes al backend:', error);
+      }
+    };
+
+    // Llama a la función de envío al backend cuando cambie el estado de messages
+    sendMessagesToBackend();
+  }, [messages]);
   return (
     <div className="chat-container ">
       <div className="mb-4">
         <span className="text-lg md:text-xl lg:text-3xl font-bold">
           <div className="flex items-center">
-            <MessageOutlined className="mr-4" />
+            <RiMessage3Fill className="mr-4 w-7 h-7 color-icons" />
             Conversación
           </div>
         </span>
@@ -65,7 +113,7 @@ const ChatComponent = () => {
         {messages.map((message) => (
           <div key={message.id} className="message">
             <div className="message-content flex">
-              <UserOutlined className="mr-4 text-xl md:text-2xl lg:text-4xl" />
+              <PiUserCircleBold className="mr-4 text-xl md:text-2xl lg:text-4xl" />
               <div>
                 <div className="flex items-center">
                   <div className="message-info">
@@ -79,7 +127,7 @@ const ChatComponent = () => {
               </div>
             </div>
             {message.repliedTo && (
-              <div className="message-reply ml-12 border-l-2 pl-2"> 
+              <div className="message-reply ml-12 border-l-2 pl-2">
                 <span className="text-gray-500 italic">Respuesta:</span>
                 <span className="ml-2">{message.repliedTo.text}</span>
               </div>
@@ -97,7 +145,16 @@ const ChatComponent = () => {
                   onClick={() => sendReply(message.id)}
                   className="ml-2 bg-transparent hover:bg-green-100 text-green-500 px-3 py-2 rounded-md"
                 >
-                  <SendOutlined className="send-icon" />
+                  <IoSend className="send-icon color-icons" />
+                </button>
+                <button
+                  onClick={() => {
+                    setReplyingTo(null);
+                    setIsReplying(false);
+                  }}
+                  className="ml-2 bg-transparent hover:bg-gray-100 text-black-500 px-3 py-2 rounded-md"
+                >
+                  <RiCloseCircleFill className="send-icon w-5 h-5" />
                 </button>
               </div>
             )}
@@ -106,7 +163,7 @@ const ChatComponent = () => {
                 className="button-respo ml-10 bg-transparent hover:bg-green-100 px-3 py-2 rounded-md text-xs font-bold"
                 onClick={() => {
                   setReplyingTo(message);
-                  setIsReplying(true); // Mostrar el input y el botón de respuesta
+                  setIsReplying(true);
                 }}
               >
                 Responder
@@ -122,15 +179,15 @@ const ChatComponent = () => {
           value={inputMessage}
           onChange={handleInputChange}
           className="flex-1 px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500 hover:border-green-500 border-2"
-          style={{ display: isReplying ? 'none' : 'block' }} // Ocultar el input si se está respondiendo
+          style={{ display: isReplying ? 'none' : 'block' }}
         />
 
         <button
           onClick={sendMessage}
           className="ml-2 bg-transparent hover:bg-green-100 text-green-500 px-3 py-2 rounded-md"
-          style={{ display: isReplying ? 'none' : 'inline-block' }} // Ocultar el botón si se está respondiendo
+          style={{ display: isReplying ? 'none' : 'inline-block' }}
         >
-          <SendOutlined className="send-icon" />
+          <IoSend className="send-icon color-icons" />
         </button>
       </div>
     </div>
