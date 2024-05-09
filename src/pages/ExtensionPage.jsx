09eb43aea4988b55e810e3404b58ae8867c7528d
Fsegurar <fsegurar@unbosque.edu.co>
2024-05-08 21:40:54 -0500

    start supletorio view

---
+++ src/pages/ExtensionPage.jsx
@@ -0,0 +1,27 @@
+import { TitleComponent } from "../components";
+import FormExtensionComponent from "../components/FormExtensionComponent";
+import UserCArdComponent from '../components/UserCardComponet';
+
+const ExtensionPage = () => {
+
+    return (
+        <div className="flex mr-5">
+            <div className="w-full m-4 ml-10 flex flex-col">
+                <div className='w-full'>
+                    <UserCArdComponent user={'Pepito Perez'} number={2}></UserCArdComponent>
+                </div>
+                <div>
+                    <TitleComponent title="Solicitud Incapacidad" />
+                </div>
+                <div>
+                    <FormExtensionComponent />
+                </div>
+            </div>
+        </div>
+
+    )
+
+
+}
+
+export default ExtensionPage;
