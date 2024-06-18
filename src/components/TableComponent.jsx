b01d9ab54440cc4fca33c744fb5e7ae39f3ba733
JuanDOsma <juosmas@unbosque.edu.co>
2024-06-17 22:54:21 -0500

    first fix bug

--- src/components/TableComponent.jsx
+++ src/components/TableComponent.jsx
@@ -1,10 +1,11 @@
-/* eslint-disable react/prop-types */
-
 import { useState, useEffect } from "react";
 import { Table, Space, Input } from "antd";
 import { FaEye } from "react-icons/fa";
 import { HiSearchCircle } from "react-icons/hi";
 import "./TableComponent.css";
+import { Button } from "antd";
+import PropTypes from 'prop-types';
+
 
 function TableComponent({ dataSource, columns, parameterAction }) {
   const filasConKey = dataSource.map((fila, index) => ({
@@ -57,13 +58,20 @@ function TableComponent({ dataSource, columns, parameterAction }) {
   return (
     <div className="mx-auto">
       <div className="boxTable border-2 border-gray-300 p-5 rounded-2xl text-5xl">
-        <div className="mb-3 w-full md:w-[40rem] ">
+        <div className="mb-3 w-full md:w-[40rem] flex">
           <Input
             placeholder="Buscar aquí..."
             value={searchText}
             onChange={(e) => setSearchText(e.target.value)}
             prefix={<HiSearchCircle className="w-8 h-8" />}
           />
+          <Button
+            to="/homePage"
+            type="primary"
+            className="color-button text-sm md:text-base lg:text-lg h-auto ml-2"
+          >
+            Buscar
+          </Button>
         </div>
         <div className="overflow-x-auto">
           <Table
@@ -79,12 +87,25 @@ function TableComponent({ dataSource, columns, parameterAction }) {
               onShowSizeChange: handlePageChange,
               pageSizeOptions: ["5", "10"],
             }}
-           
+
           />
         </div>
       </div>
     </div>
   );
 }
+
+TableComponent.propTypes = {
+  dataSource: PropTypes.arrayOf(PropTypes.object).isRequired,
+  columns: PropTypes.arrayOf(
+    PropTypes.shape({
+      key: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+      dataIndex: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  parameterAction: PropTypes.func,
+};
+
 
 export default TableComponent;
