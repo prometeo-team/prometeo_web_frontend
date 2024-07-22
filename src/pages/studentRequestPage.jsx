249faaf6cb157689fe32d41310bd625ac31edcb4
gitForDavidL <davidtach26@gmail.com>
2024-07-21 20:03:33 -0500

    integrated wiht get endpoint

--- src/pages/studentRequestPage.jsx
+++ src/pages/studentRequestPage.jsx
@@ -1,52 +1,88 @@
 import { TitleComponent, TableComponent } from '../components/';
 import { Tag } from 'antd';
-import './studentRequestPage.css'
+import './studentRequestPage.css';
 import { ArrowLeftOutlined } from '@ant-design/icons';
 import { Button } from 'antd';
 import UserCardComponent from '../components/UserCardComponet';
-
+import { useState, useEffect } from 'react';
+import { getInfoToken } from '../utils/utils';
 const studentRequestPage = () => {
+
+  const [filas, setFilas] = useState([]);
+
+
+  useEffect(() => {
+    const userInfo = getInfoToken();
+    console.log('userInfo:', userInfo);
+    const obtenerDatos = async () => {
+      try {
+        const response = await fetch(`http://127.0.0.1:3030/api/request/getAllRequest?page=1&username=${userInfo.sub}`, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${sessionStorage.getItem('token')}`,
+          },
+        });
+        const result = await response.json();
+        setFilas(result.data.content);
+        console.log('Datos obtenidos:', result.data.content[0].requestTypeEntity);
+     
+      } catch (error) {
+        console.error("Error al obtener los datos:", error);
+      
+      }
+    };
+    obtenerDatos();
+  }, []);
+
 
   const columns = [
     {
       title: 'Solicitud',
-      dataIndex: 'id_solicitud',
-      key: 'id_solicitud',
+      dataIndex: 'idRequest',
+      key: 'idRequest',
     },
     {
       title: 'Fecha de Creacion',
-      dataIndex: 'fecha',
-      key: 'fecha',
-      sorter: (a, b) => new Date(a.fecha) - new Date(b.fecha),
-
+      dataIndex: 'createdAt',
+      key: 'createdAt',
+      sorter: (a, b) => new Date(a.createdAt) - new Date(b.createdAt),
+      render: (text) => {
+        const date = new Date(text);
+        const formattedDate = date.toLocaleDateString('es-ES', {
+          day: '2-digit',
+          month: '2-digit',
+          year: 'numeric',
+        });
+        return formattedDate;
+      },
     },
     {
       title: 'Estado de la solicitud',
       key: 'estado',
-      dataIndex: 'estado',
+      dataIndex: 'estado', // Assuming you will add this field to the response
       render: (estado) => {
         let color;
         if (estado === 'Iniciado') {
           color = 'green';
         } else if (estado === 'Proceso') {
-          color = 'yellow';
-        } else {
-          (estado === 'Finalizado')
+          color = '#F1C40F';
+        } else if (estado === 'Finalizado') {
           color = 'red';
         }
-        return (
-          <Tag color={color}>
-            {estado.toUpperCase()}
-          </Tag>
-        );
+        return <Tag color={color}>{estado ? estado.toUpperCase() : 'DESCONOCIDO'}</Tag>;
       },
-
     },
     {
       title: 'Tipo de Solicitud',
-      dataIndex: 'tipo_solicitud',
       key: 'tipo_solicitud',
-    }
+      render: (text, record) => record.requestTypeEntity.nameType,
+    },
+    {
+      title: 'Programa de Estudiante',
+      dataIndex: 'programStudent',
+      key: 'programStudent',
+    },
   ];
 
   const handleView = (e, record) => {
@@ -55,101 +91,20 @@ const studentRequestPage = () => {
     console.log('Ver registro:', record.id_solicitud);
   };
 
-  /*const [filas, setFilas] = useState([]);
-
-  useEffect(() => {
-    // Obtener los datos del JSON y almacenarlos en la variable de estado 'filas'
-    const obtenerDatos = async () => {
-      const response = await fetch('/api/datos');
-      const data = await response.json();
-      setFilas(data);
-    };
-    obtenerDatos();
-  }, []);*/
-
-  const dataSource = [
-    {
-      id_solicitud: 'C231231',
-      fecha: '2023-03-15',
-      estado: 'Finalizado',
-      tipo_solicitud: 'Reintegro'
-    },
-    {
-      id_solicitud: 'C231231',
-      fecha: '2023-04-17',
-      estado: 'Proceso',
-      tipo_solicitud: 'Reintegro'
-    },
-    {
-      id_solicitud: 'C231231',
-      fecha: '2023-03-16',
-      estado: 'Iniciado',
-      tipo_solicitud: 'Reintegro'
-    },
-    {
-      id_solicitud: 'C231231',
-      fecha: '2023-03-16',
-      estado: 'Iniciado',
-      tipo_solicitud: 'Reintegro'
-    },
-    {
-      id_solicitud: 'C231231',
-      fecha: '2023-03-16',
-      estado: 'Iniciado',
-      tipo_solicitud: 'Reintegro'
-    },
-    {
-      id_solicitud: 'C231231',
-      fecha: '2023-03-16',
-      estado: 'Iniciado',
-      tipo_solicitud: 'Reintegro'
-    },
-    {
-      id_solicitud: 'C231231',
-      fecha: '2023-03-16',
-      estado: 'Iniciado',
-      tipo_solicitud: 'Reintegro'
-    },
-    {
-      id_solicitud: 'C231231',
-      fecha: '2023-03-16',
-      estado: 'Iniciado',
-      tipo_solicitud: 'Reintegro'
-    },
-    {
-      id_solicitud: 'C231231',
-      fecha: '2023-03-16',
-      estado: 'Iniciado',
-      tipo_solicitud: 'Reintegro'
-    },
-    {
-
-      id_solicitud: 'C231231',
-      fecha: '2023-03-16',
-      estado: 'Iniciado',
-      tipo_solicitud: 'Reintegro'
-    },
-    {
-      id_solicitud: 'C231231',
-      fecha: '2023-03-16',
-      estado: 'Iniciado',
-      tipo_solicitud: 'Reintegro'
-    },
-  ];
 
   return (
     <div className="h-screen scroll-container ml-4">
-      <UserCardComponent  number={2} />
+      <UserCardComponent number={2} />
       <div>
         <TitleComponent title={'Mis Solicitudes'} />
       </div>
 
       <div className="m-5">
-        <TableComponent dataSource={dataSource} columns={columns} parameterAction={handleView} />
-        
+        <TableComponent dataSource={filas} columns={columns} parameterAction={handleView} />
+
       </div>
       <div className="ml-5 mb-5">
-         <Button to="/homePage" type="primary" className='color-button text-sm md:text-base lg:text-lg h-auto' icon={<ArrowLeftOutlined />}>Volver</Button>
+        <Button to="/homePage" type="primary" className='color-button text-sm md:text-base lg:text-lg h-auto' icon={<ArrowLeftOutlined />}>Volver</Button>
       </div>
     </div>
   )
---
+++ src/utils/utils.jsx
@@ -0,0 +1,20 @@
+export const  getInfoToken = () => {
+    const token = sessionStorage.getItem('token');
+    if (token) {
+      try {
+        const base64Url = token.split('.')[1];
+        const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+        const jsonPayload = decodeURIComponent(
+          atob(base64)
+            .split('')
+            .map(c => '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2))
+            .join('')
+        );
+        const decodedToken = JSON.parse(jsonPayload);
+        return decodedToken
+      } catch (error) {
+        console.error('Error decoding token:', error);
+      }
+    }
+
+}
