a98c70c32fb8e2845eacd23052e6a7e415b92b77
Fsegurar <fsegurar@unbosque.edu.co>
2024-08-26 22:12:20 -0500

    conection success full

--- src/components/ModalOtherRequestComponent.jsx
+++ src/components/ModalOtherRequestComponent.jsx
@@ -1,26 +1,32 @@
 import { useState } from "react";
-import { Modal, Button } from "antd";
+import { Modal, Button, message, Input } from "antd";
 import PropTypes from "prop-types";
-import UploadDocumentComponent from "./UploadDocumentComponent";
+import { AiFillFilePdf } from "react-icons/ai";
+import { MdCloudUpload, MdDelete } from "react-icons/md";
+import "./UploadDocumentComponent.css";
 
 const ModalOtherRequestComponent = ({ visible, onClose, setDocuments }) => {
   const [confirmLoading, setConfirmLoading] = useState(false);
-  const [pdf, setPdf] = useState(null);
-  const [fileName, setFileName] = useState("Archivo no seleccionado");
+  const [filesArr, setFilesArr] = useState([
+    { id: "file0", pdf: null, fileName: "Archivo no seleccionado", originalfile: null }
+  ]);
 
   const handleOk = () => {
     setConfirmLoading(true);
     setTimeout(() => {
-      const documentsWithNames = [
-        pdf && { url: pdf, name: fileName },
-      ].filter(doc => doc !== null);
+      const documentsWithNames = filesArr
+        .filter((file) => file.pdf)
+        .map((file) => {
+          console.log(file); // Agrega este log para depuración
+          return { url: file.pdf, name: file.fileName, originalfile: file.originalfile };
+        });
+  
       setDocuments(documentsWithNames);
       onClose();
       setConfirmLoading(false);
     }, 1000);
   };
-
-  const allFilesSelected = pdf;
+  
 
   const handleCancel = () => {
     document.querySelector(".center-modal").classList.add("animate__zoomOut");
@@ -29,21 +35,63 @@ const ModalOtherRequestComponent = ({ visible, onClose, setDocuments }) => {
     }, 500);
   };
 
-
-  const handleFileChange = (file) => {
-    setFileName(file.name);
-    setPdf(URL.createObjectURL(file));
+  const handleFileChange = (id, event) => {
+    const file = event.target.files[0];
+    if (file) {
+      if (file.size > 10 * 1024 * 1024) {
+        message.error(`${file.name}: El archivo no debe pesar más de 10MB`, 10);
+        return;
+      }
+      if (!file.type.includes("pdf")) {
+        message.error(`Por favor, seleccione un archivo PDF`, 3);
+        return;
+      }
+      setFilesArr((prevFilesArr) =>
+        prevFilesArr.map((fileObj) =>
+          fileObj.id === id
+            ? { ...fileObj, fileName: file.name, pdf: URL.createObjectURL(file), originalfile: file }
+            : fileObj
+        )
+      );
+    }
   };
 
-  const handleDelete = () => {
-    setFileName("Archivo no seleccionado");
-    setPdf(null);
+  const handleDelete = (id) => {
+    setFilesArr((prevFilesArr) =>
+      prevFilesArr.map((fileObj) =>
+        fileObj.id === id
+          ? { ...fileObj, fileName: "Archivo no seleccionado", pdf: null, originalfile: null }
+          : fileObj
+      )
+    );
   };
+  
+
+  const handlePlusButton = () => {
+    if (filesArr.length < 5) {
+      const newId = `file${filesArr.length}`;
+      setFilesArr((prevFilesArr) => [
+        ...prevFilesArr,
+        { id: newId, pdf: null, fileName: "Archivo no seleccionado" }
+      ]);
+    } else {
+      message.warning("Solo se pueden agregar hasta 5 archivos.");
+    }
+  };
+
+  const handleMinusButton = () => {
+    if (filesArr.length > 1) {
+      setFilesArr((prevFilesArr) => prevFilesArr.slice(0, -1));
+    }
+  };
+
+  // Check if the first file in the array has a PDF loaded
+  const isFirstFileLoaded = filesArr[0].pdf;
 
   return (
     <>
       {visible && (
-        <div className="modal-backdrop ">
+        <div className="modal-backdrop">
           <Modal
             open={visible}
             onOk={handleOk}
@@ -52,31 +100,74 @@ const ModalOtherRequestComponent = ({ visible, onClose, setDocuments }) => {
             footer={null}
             closable={false}
             centered
-            wrapClassName="center-modal animate__animated animate__zoomIn "
+            wrapClassName="center-modal animate__animated animate__zoomIn"
           >
             <div className="text-center mb-4">
               <h4 className="text-lg font-bold">Documento - Formato .pdf*</h4>
             </div>
             <div className="grid">
-              <UploadDocumentComponent
-                onChange={handleFileChange}
-                pdf={pdf}
-                fileName={fileName}
-                onDelete={handleDelete}
-                clickClassNameP=".input-field"
-                clickClassName="input-field"
-                label="Documento"
-                isRequired={true}
-              />
+              {filesArr.map((fileObj) => (
+                <div key={fileObj.id} className="text-center">
+                  <div>
+                    <p className="text-sm font-bold mb-1">Documento</p>
+                    <p className="text-xs text-gray-600 mb-4"></p>
+                  </div>
+                  <div
+                    className="flex justify-center items-center border-dashed border-2 cursor-pointer rounded-md form-color h-20"
+                    onClick={() => document.getElementById(fileObj.id).click()}
+                  >
+                    <Input
+                      id={fileObj.id}
+                      type="file"
+                      accept=".pdf"
+                      className="input-field"
+                      hidden
+                      onChange={(event) => handleFileChange(fileObj.id, event)}
+                    />
+                    {fileObj.pdf ? (
+                      <div className="flex items-center">
+                        <AiFillFilePdf color="#000" size={48} />
+                        <p>{fileObj.fileName.length > 20 ? `${fileObj.fileName.slice(0, 17)}...${fileObj.fileName.split('.').pop()}` : fileObj.fileName}</p>
+                      </div>
+                    ) : (
+                      <MdCloudUpload color="#000" size={50} />
+                    )}
+                  </div>
+                  <span className="flex items-center uploaded-row">
+                    {fileObj.fileName === "Archivo no seleccionado" ? (
+                      "Archivo no seleccionado"
+                    ) : (
+                      <>
+                        Eliminar archivo -
+                        <MdDelete className="delete-icon" onClick={() => handleDelete(fileObj.id)} />
+                      </>
+                    )}
+                  </span>
+                </div>
+              ))}
             </div>
             <div className="text-center mt-2">
-              {pdf ? (
-                <Button key="submit" onClick={handleOk} className="text-white mx-auto custom-btn">
-                  Cargar documento
+              {filesArr.some((file) => file.pdf) ? (
+                <Button onClick={handleOk} className="text-white mx-auto custom-btn">
+                  Cargar documentos
                 </Button>
               ) : (
                 <Button disabled className="text-white mx-auto custom-btn">
-                  Cargar documento
+                  Cargar documentos
+                </Button>
+              )}
+              {isFirstFileLoaded && (
+                <Button 
+                  onClick={handlePlusButton} 
+                  className="text-white mx-auto custom-btn" 
+                  disabled={filesArr.length >= 5}
+                >
+                  Añadir otro
+                </Button>
+              )}
+              {filesArr.length > 1 && (
+                <Button onClick={handleMinusButton} className="text-white mx-auto custom-btn">
+                  Eliminar último
                 </Button>
               )}
             </div>
--- src/pages/OtherRequestPage.jsx
+++ src/pages/OtherRequestPage.jsx
@@ -1,11 +1,39 @@
+import React, { useEffect, useState } from "react";
 import { TitleComponent } from "../components";
+import { Link, useNavigate } from 'react-router-dom';
 import FormOtherRequestComponent from "../components/FormOtherRequestComponent";
 import UserCArdComponent from '../components/UserCardComponet';
+import ModalAskCarrer from '../components/ModalAskCarrer.jsx';
 import '../App.css'
-
+var carrera;
 const OtherRequestPage = () => {
 
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    useEffect(() => {
+       
+            setIsModalOpen(true);
+        
+      }, []);
+    
+
+    const handleModalConfirm = () => {
+        carrera = document.getElementById('carrer_select').value;
+        sessionStorage.setItem('Carrera', carrera);
+        console.log('prueba osama'+carrera);
+        setIsModalOpen(false);
+    };
+
+    const handleModalCancel = () => {
+        setIsModalOpen(false);
+     };
+
     return (
+        <>
+        <ModalAskCarrer
+            isVisible={isModalOpen}
+            onConfirm={handleModalConfirm}
+            onCancel={handleModalCancel}
+         />
         <div className="h-screen scroll-container">
                 <div className='w-full mt-4'>
                     <UserCArdComponent user={'Pepito Perez'} number={2}></UserCArdComponent>
@@ -14,9 +42,10 @@ const OtherRequestPage = () => {
                     <TitleComponent title="Otras Solicitudes" />
                 </div>
                 <div>
-                    <FormOtherRequestComponent />
+                    <FormOtherRequestComponent carrer={carrera}/>
                 </div>
         </div>
+        </>
 
     )
 
