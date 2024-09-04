cbb68647b7ab4487a46cd6a885dc205e6ded9a08
Fsegurar <fsegurar@unbosque.edu.co>
2024-09-04 17:41:25 -0500

    navigate

--- src/pages/infoAdminRequestPage.jsx
+++ src/pages/infoAdminRequestPage.jsx
@@ -4,10 +4,11 @@ import NavbarTypeComponent from '../components/NavbarTypeComponent';
 import TableComponent from '../components/TableComponent';
 import CardGraficsComponent from '../components/CardGraficsComponent';
 import { Tag } from 'antd';
+import { useNavigate } from 'react-router-dom';
 import '../App.css';
 
 const InfoAdminRequestPage = () => {
-
+  const navigate = useNavigate();
   const [filas, setFilas] = useState([]);
   const [Pendiente, setPendiente] = useState([]);
   const [proceso, setProceso] = useState([]);
@@ -133,8 +134,8 @@ const InfoAdminRequestPage = () => {
 
   const handleView = (e, record) => {
     e.preventDefault();
-    // Aquí se puede agregar la lógica para ver el registro
-    console.log('Ver registro:', record.id_solicitud);
+    navigate(`/admin/solicitud?id=${record.id_solicitud}&tipo=${record.tipo_solicitud}`);
+
   };
   
   const handleClickType = (option) => {
@@ -163,7 +164,7 @@ const InfoAdminRequestPage = () => {
       obtenerDatos('');
     }else{
     fetchGrafics('?requestType=' + option);
-    obtenerDatos('?nameType=' + option);
+    obtenerDatos('&nameType=' + option);
     }
   };
 
