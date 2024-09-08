eb32eafb537db68d720ebaf360e08cda9faf07ce
JuanDOsma <juosmas@unbosque.edu.co>
2024-09-08 14:51:18 -0500

    Not aproved

--- src/components/AdminInfoRrequest.jsx
+++ src/components/AdminInfoRrequest.jsx
@@ -12,6 +12,7 @@ const ComponentInfoSR = () => {
   const [initialStatus, setInitialStatus] = useState('');
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [initialStatusValue, setInitialStatusValue] = useState('');
+  const [additionalInfo, setAdditionalInfo] = useState('');
 
 
   const url = window.location.href;
@@ -95,13 +96,21 @@ const ComponentInfoSR = () => {
       try {
         const userInfo = JSON.parse(sessionStorage.getItem('userInfo'));
         const username = userInfo ? userInfo.sub : '';
-        const response = await fetch(`https://prometeo-backend-e8g5d5gydzgqezd3.eastus-01.azurewebsites.net/api/request/updateStatusRequest?idRequest=${id}&status=${selectedStatus}&username=${username}`, {
+
+        let putUrl = `https://prometeo-backend-e8g5d5gydzgqezd3.eastus-01.azurewebsites.net/api/request/updateStatusRequest?idRequest=${id}&status=${selectedStatus}&username=${username}`;
+
+        if (selectedStatus === 'No valida') {
+          putUrl += `&msgNotApproved=${encodeURIComponent(additionalInfo)}`;
+        }
+
+        const response = await fetch(putUrl, {
           method: 'PUT',
           headers: {
             'Content-Type': 'application/json',
             Authorization: `Bearer ${sessionStorage.getItem('token')}`,
           },
         });
+
         if (response.ok) {
           notification.success({
             message: 'Estado actualizado',
@@ -136,6 +145,10 @@ const ComponentInfoSR = () => {
 
   const handleSelectChange = (value) => {
     setSelectedStatus(value);
+  };
+
+  const handleAdditionalInfoChange = (e) => {
+    setAdditionalInfo(e.target.value);
   };
 
   if (isLoading) {
@@ -194,16 +207,40 @@ const ComponentInfoSR = () => {
             </select>
           </div>
         </Descriptions.Item>
+        {selectedStatus === 'No Aprobado' ? (
+          <Descriptions.Item className="ml-4 w-full md:w-2/3">
+            <div className="flex flex-col items-start justify-between w-full">
+              <div className="flex flex-col w-full">
+                <h4 className="text-sm font-bold ml-5 mb-2">Razón de invalidez:</h4>
+                <textarea
+                  className="p-4 w-full h-24 mr-6 text-xs md:text-sm rounded-lg shadow-md resize-none overflow-auto border-2 border-[#43737E]"
+                  placeholder="Ingrese la razón de invalidez"
+                  value={additionalInfo}
+                  onChange={handleAdditionalInfoChange}
+                  rows={4}
+                />
 
-        <Descriptions.Item className="ml-4 w-full md:w-1/3">
-          <Button
-            type="primary"
-            className="custom-btn -mt-6 ml-4 w-full h-9 text-xs md:text-sm text-white rounded-lg shadow-md color-button font-bold flex items-center justify-center"
-            onClick={() => setIsModalVisible(true)}
-          >
-            Confirmar <FaCheck className="ml-2 md:ml-4" />
-          </Button>
-        </Descriptions.Item>
+              </div>
+              <Button
+                type="primary"
+                className="custom-btn mt-4 w-full h-9 text-xs md:text-sm text-white rounded-lg shadow-md color-button font-bold flex items-center justify-center"
+                onClick={() => setIsModalVisible(true)}
+              >
+                Confirmar <FaCheck className="ml-2" />
+              </Button>
+            </div>
+          </Descriptions.Item>
+        ) : (
+          <Descriptions.Item className="ml-4 w-full md:w-1/3">
+            <Button
+              type="primary"
+              className="custom-btn -mt-6 ml-4 w-full h-9 text-xs md:text-sm text-white rounded-lg shadow-md color-button font-bold flex items-center justify-center"
+              onClick={() => setIsModalVisible(true)}
+            >
+              Confirmar <FaCheck className="ml-2 md:ml-4" />
+            </Button>
+          </Descriptions.Item>
+        )}
       </Descriptions>
 
       <Modal
