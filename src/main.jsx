e5d87a41ea2cd63b90c5b82e1b7625f0ec48c658
gitForDavidL <davidtach26@gmail.com>
2024-09-15 22:49:33 -0500

    removed log enviroment

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,6 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 
 
-console.log("la variable es: " , import.meta.env.VITE_API_URL);
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
