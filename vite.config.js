3c2d54832ed7a30d41ee50376a7c79237bd6f214
gitForDavidL <davidtach26@gmail.com>
2024-09-15 23:09:18 -0500

    added vite-plugin-static-copy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+  viteStaticCopy({
+    targets: [
+      {
+        src: '_redirects',
+        dest: ''
+      }
+    ]
+  })
+  ],
 })
